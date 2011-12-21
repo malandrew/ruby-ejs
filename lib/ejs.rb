dd29d0e7d0491b5688fa3c7ce0b771f7cdca41f3
Andrew de Andrade <andrew@deandrade.com.br>
2011-12-21 02:08:55 -0500

    Upgrading to Tilt and registering with sprockets

--- lib/ejs.rb
+++ lib/ejs.rb
@@ -1,69 +1,13 @@
-# EJS (Embedded JavaScript) template compiler for Ruby
-# (c) 2011 Sam Stephenson
-#
-# This is a port of Underscore.js' `_.template` function:
-# http://documentcloud.github.com/underscore/
+require "ejs/version"
 
 module EJS
-  class << self
-    attr_accessor :evaluation_pattern
-    attr_accessor :interpolation_pattern
-
-    # Compiles an EJS template to a JavaScript function. The compiled
-    # function takes an optional argument, an object specifying local
-    # variables in the template.  You can optionally pass the
-    # `:evaluation_pattern` and `:interpolation_pattern` options to
-    # `compile` if you want to specify a different tag syntax for the
-    # template.
-    #
-    #     EJS.compile("Hello <%= name %>")
-    #     # => "function(obj){...}"
-    #
-    def compile(source, options = {})
-      source = source.dup
-
-      replace_evaluation_tags!(source, options)
-      escape_quotes!(source)
-      escape_whitespace!(source)
-
-      "require('ejs').compile('#{source}')"
-    end
-
-    # Evaluates an EJS template with the given local variables and
-    # compiler options. You will need the ExecJS
-    # (https://github.com/sstephenson/execjs/) library and a
-    # JavaScript runtime available.
-    #
-    #     EJS.evaluate("Hello <%= name %>", :name => "world")
-    #     # => "Hello world"
-    #
-    def evaluate(template, locals = {}, options = {})
-      require "execjs"
-      code = File.read(File.join(File.dirname(__FILE__),'ejs.js'))
-      code += "\n\nvar evaluate = #{compile(template, options)}"
-      context = ExecJS.compile(code)
-      context.call("evaluate", locals)
-    end
-
-    protected
-      def escape_quotes!(source)
-        source.gsub!(/\\/) { '\\\\' }
-        source.gsub!(/'/) { "\\'" }
-      end
-
-      def replace_evaluation_tags!(source, options)
-        source.gsub!(options[:evaluation_pattern] || evaluation_pattern) do
-          "<%" + $1.gsub(/\\'/, "'").gsub(/[\r\n\t]/, ' ') + "%>"
-        end
-      end
-
-      def escape_whitespace!(source)
-        source.gsub!(/\r/, '\\r')
-        source.gsub!(/\n/, '\\n')
-        source.gsub!(/\t/, '\\t')
-      end
+  autoload(:EJS, 'ejs/ejs')
+  autoload(:TiltEJS, 'ejs/tilt_ejs')
+  
+  if defined?(Rails)
+    require 'ejs/engine'
+  else
+    require 'sprockets'
+    Sprockets.register_engine '.ejs', TiltEJS
   end
-
-  self.evaluation_pattern = /<%([\s\S]+?)%>/
-  self.interpolation_pattern = /<%=([\s\S]+?)%>/
 end
